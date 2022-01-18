e34c32223508a731c823eb8acd86245367757894
tsukasaki6805 <kohei.tsuka46@gmail.com>
2022-01-18 07:52:25 +0000

    Add Coutomer

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,49 @@
 class Public::CustomersController < ApplicationController
+   before_action :ensure_correct_customer, only: [:edit, :update]
+
+  def show
+    @customer = Customer.find(params[:id])
+    @posts = @customer.posts.page(params[:page]).reverse_order
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+    if @customer != current_customer
+      redirect_to customer_path
+    end
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    if @customer.update(customer_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to customer_path(@customer)
+    else
+      render 'edit'
+    end
+  end
+
+  def unsubscribe
+    @customer = Customer.find_by(name: params[:name])
+  end
+
+  def withdraw
+    @customer = Customer.find_by(name: params[:name])
+    @customer.update(is_valid: false)
+    reset_session
+    redirect_to root_path
+  end
+
+  private
+  def customer_params
+    params.require(:customer).permit(:name, :name_kana, :post_code, :address, :telephon_number, :email)
+  end
+
+  def ensure_correct_customer
+    @customer = Customer.find_by(id:params[:id])
+    if @customer.id != current_customer.id
+      redirect_to customer_path(current_customer)
+    end
+  end
+
 end
