88e0430c4d010e254de7ecd7cc9f2aff1bf76bb8
EC2 Default User <ec2-user@ip-172-31-89-58.ec2.internal>
2022-01-20 00:24:51 +0000

    商品新規登録、一覧、詳細

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,8 +1,40 @@
 class Admin::ProductsController < ApplicationController
 
-private
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to admin_products_path
+    else
+      render :new
+    end
+  end
+
+  def index
+    @products = Product.all
+  end
+
+  def show
+    @product = Product.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if @product.update(product_params)
+      redirect_to admin_product_path
+    else
+      render :edit
+    end
+  end
+
+  private
   def product_params
-    params.require(:product).permit(:name, :body, :genre_id)
+    params.require(:product).permit(:image, :name, :explanation, :genre_id, :selling_price, :is_selling)
   end
 
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,6 +5,7 @@ class Product < ApplicationRecord
   has_many :orders, through: :order_datails
   has_many :order_details
 
+  enum is_selling: { on_sale: 0, no_sale: 1}
   attachment :image
 
   def with_tax_price
