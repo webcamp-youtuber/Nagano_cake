a3525ac5898a3ccab8ebf71e6a014dffbf75ede2
EC2 Default User <ec2-user@ip-172-31-92-40.ec2.internal>
2022-01-22 00:43:07 +0000

    修正中

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,55 @@
+class Public::CartItemsController < ApplicationController
+
+  def index
+    @cart_items = CartItem.all
+    @total = 0
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.public_customer_id = current_public_customer.id
+
+    if current_customer.cart_products.find_by(product_id: params[:cart_item][:product_id]).present?
+      @cart_item = current_customer.cart_products.find_by(product_id: params[:cart_item][:product_id])
+      @cart_item.quantity += params[:cart_item][:quantity].to_i
+      @cart_item.save
+      redirect_to public_cart_items_path
+    else
+      @cart_item.save
+      redirect_to public_cart_items_path
+    end
+
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to public_cart_items_path
+    else
+      @cart_items = CartItem.all
+      render :index
+    end
+  end
+
+
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to public_cart_items_path
+  end
+
+  def all_destroy
+    @cart_items = current_customer.cart_items.destroy_all
+    redirect_to public_cart_items_path
+  end
+
+
+  private
+
+  def cart_item_params
+      params.require(:cart_item).permit(:product_id, :quantity)
+  end
+
+end
+
